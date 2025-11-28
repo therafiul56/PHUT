af0d074e3491c4dcf4383ef421e9bb6126d06f49
MD. Rafiul Islam <rafiulislam316@gmail.com>
2025-11-28 11:04:45 +0600

    Add example of string iterators in C++
    Demonstrate usage of string iterators with begin() and end() methods.

---
+++ Introduction to C++ for DSA/module 5/itarator /str.begin(),str.end().cpp
@@ -0,0 +1,12 @@
+// itaratords of  string str.begin(),str.end();
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	string s1 = "hello world";
+	string s2 = "monkey";
+	cout << *s1.begin() << endl << *(s1.end()-1) << endl;
+	cout << s1 << endl ; 
+	return 0;
+
+}
