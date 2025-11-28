328735ca27f9f5ad184bdc7a602459960e14c635
MD. Rafiul Islam <rafiulislam316@gmail.com>
2025-11-28 10:44:55 +0600

    Implement example for s.erase() in C++
    Add example of s.erase() function usage in C++

---
+++ Introduction to C++ for DSA/module 5/stringmdifires/s.erase().cpp
@@ -0,0 +1,12 @@
+// s.erase function deletes element after the parameter
+// syntax variable name.erase(initial,size_iwanto_erase);
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	string s1 = "hello w world";
+	s1.erase(5,2);
+	cout << s1 << endl ; 
+	return 0;
+
+}
