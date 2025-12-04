d9ab3dfe5ee3edf9df2c0bad505b97af3e29b7f4
MD. Rafiul Islam <rafiulislam316@gmail.com>
2025-12-04 14:41:45 +0600

    Add C++ program to sort array of student marks

--- Introduction to C++ for DSA/moodule /sort array of marks/with build in function.c
+++ Introduction to C++ for DSA/moodule /sort array of marks/with build in function.c
@@ -1,30 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-class student{
-    public:
-  string nm;
-  int rl;
-  int mrks;
-};
-bool cmp(student l , student r){
-    if(l.mrks > r.mrks){
-        return true;
-    }
-    else {
-        return false;
-    }
-}
-int main() {
-	int a;
-	cin >> a;
-	student arr[a];
-	for(int i = 0 ; i<a ; i++){
-	    cin >> arr[i].nm >> arr[i].rl >> arr[i].mrks ;
-	}
-    sort(arr,arr+a,cmp);
-    for(int i = 0 ; i<a ; i++){
-        arr[i].rl = i+1;
-	    cout << arr[i].nm << " "<<  arr[i].rl  << " " << arr[i].mrks << endl ;
-	}
 
+class student {
+public:
+    string nm;
+    int rl;
+    int mrks;
+};
+
+bool cmp(const student &l, const student &r) {
+    return l.mrks > r.mrks;
+}
+
+int main() {
+    int a;
+    cin >> a;
+    vector<student> arr(a);
+
+    for (int i = 0; i < a; i++)
+        cin >> arr[i].nm >> arr[i].rl >> arr[i].mrks;
+
+    sort(arr.begin(), arr.end(), cmp);
+
+    for (int i = 0; i < a; i++) {
+        arr[i].rl = i + 1;
+        cout << arr[i].nm << " " << arr[i].rl << " " << arr[i].mrks << endl;
+    }
 }
